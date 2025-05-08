4a73b56e34c06c4291836b3f9a7ce32bb9a313b5
Malen <malen.izetaegizabal@usp.ceu.es>
2025-05-08 22:32:24 +0200

    He cambiado el main para que al ejecutarse te redirija al menu

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-# main.py
-
-import threading
-import tkinter as tk
-from gui.menu import MenuGUI
-
-def iniciar_servidor_local():
-    from server.server import iniciar_servidor
-    hilo_servidor = threading.Thread(target=iniciar_servidor, daemon=True)
-    hilo_servidor.start()
-
-def main():
-    # Iniciar el servidor localmente
-    iniciar_servidor_local()
-
-    # Crear y mostrar la GUI principal
-    root = tk.Tk()
-    root.title("Máquina Arcade Distribuida con IA")
-    app = MenuGUI(master=root)
-    app.mainloop()
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from gui import menu as menu_gui
 
 if __name__ == "__main__":
-    main()
+    menu_gui.main()
