6ff5ec1682914518bc13028dcb6b30d25322a293
Malen <malen.izetaegizabal@usp.ceu.es>
2025-05-08 19:14:44 +0200

    He cambiado la interfaz de caballo_gui para que tuviese relación con mario bros

--- gui/caballo_gui.py
+++ gui/caballo_gui.py
@@ -6,6 +6,10 @@ from games import caballo_tour as ct
 def manual_knights_tour(n):
     pygame.init()
     cell_size = 60
+    yoshi_image = pygame.image.load(os.path.join(os.path.dirname(__file__), "../assets/images/yoshi.png"))
+    yoshi_image = pygame.transform.scale(yoshi_image, (cell_size, cell_size)) 
+    huevo_image = pygame.image.load(os.path.join(os.path.dirname(__file__), "../assets/images/huevo.png"))
+    huevo_image = pygame.transform.scale(huevo_image, (50, 60))
     screen = pygame.display.set_mode((n * cell_size, n * cell_size))
     pygame.display.set_caption("Recorrido del Caballo")
     font = pygame.font.SysFont(None, 24)
@@ -18,12 +22,16 @@ def manual_knights_tour(n):
         for row in range(n):
             for col in range(n):
                 rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
-                color = (255, 255, 255) if (row + col) % 2 == 0 else (0, 0, 0)
+                color = (255, 255, 255) if (row + col) % 2 == 0 else (104, 224, 84)
                 pygame.draw.rect(screen, color, rect)
-                pygame.draw.rect(screen, (100, 100, 100), rect, 1)
+                #pygame.draw.rect(screen, (100, 100, 100), rect, 1)
                 if board[row][col] != -1:
-                    text = font.render(str(board[row][col]), True, (255, 0, 0))
-                    screen.blit(text, (col * cell_size + 5, row * cell_size + 5))
+                    screen.blit(huevo_image, (col * cell_size, row * cell_size))
+                    move_text = font.render(str(board[row][col]), True, (0, 100, 0))  
+                    text_rect = move_text.get_rect(center=(col * cell_size + cell_size // 2, row * cell_size + cell_size // 2))
+                    screen.blit(move_text, text_rect)
+        if current_pos:
+            screen.blit(yoshi_image, (current_pos[1] * cell_size, current_pos[0] * cell_size))
         pygame.display.flip()
 
     running = True
